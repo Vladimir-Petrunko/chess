93b1924d4aa5f684849f037b700a6ba18b5725c3
Vladimir Petrunko <vladimirr.sapfire13@gmail.com>
2021-06-27 04:05:20 +0300

    Added javadoc

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -14,8 +14,15 @@ import static utils.Global.SIZE;
 public class Pawn extends Piece {
     private static final List<Pair> whiteDeltas = List.of(new Pair(1, 0));
     private static final List<Pair> blackDeltas = List.of(new Pair(-1, 0));
-    private final int dir, nextPromotion;
 
+    private final int dir; // direction of motion: -1 (to decreasing row indices) or 1 (to increasing row indices)
+    private final int nextPromotion; // index of row just preceding promotion row
+
+    /**
+     * Constructor of {@code Pawn} of a given color.
+     *
+     * @param color the color of this pawn
+     */
     public Pawn(Color color) {
         super(color);
         dir = (color == Color.WHITE ? -1 : 1);
@@ -28,40 +35,55 @@ public class Pawn extends Piece {
     }
 
     @Override
-    public boolean validMoveDelta(final int dr, final int dc) {
+    public boolean validMoveDelta(int dr, int dc) {
         return dr == dir && dc == 0;
     }
 
     @Override
-    public boolean validAttackDelta(final int dr, final int dc) {
+    public boolean validCaptureDelta(int dr, int dc) {
         return dr == dir && Math.abs(dc) == 1;
     }
 
-    private List<Move> checkPromotion(final boolean condition, final Cell initial, final Cell target) {
+    /**
+     * Private method that returns a list of all possible promotion moves of this pawn with a specified source and
+     * destination cell, provided some initial condition is true.<br><br>
+     *
+     * The condition can be anything, as the method is purely auxiliary and serves to avoid code duplication.
+     * Ideologically, the condition should represent the legality of this particular pawn promotion.
+     *
+     * @param condition a condition that should evaluate to {@code true}
+     * @param initial the initial cell of the pawn
+     * @param target the destination cell of the pawn
+     *
+     * @return a list of all possible promotion moves of this pawn from {@code initial} to {@code target}
+     */
+    private List<Move> checkPromotion(boolean condition, Cell initial, Cell target) {
         if (condition) {
-            List<Move> list = new ArrayList<>();
-            list.add(new Move(initial, target, MoveCategory.PROMOTE_TO_QUEEN));
-            list.add(new Move(initial, target, MoveCategory.PROMOTE_TO_ROOK));
-            list.add(new Move(initial, target, MoveCategory.PROMOTE_TO_BISHOP));
-            list.add(new Move(initial, target, MoveCategory.PROMOTE_TO_KNIGHT));
-            return list;
+            return List.of(
+                new Move(initial, target, null, MoveCategory.PROMOTE_TO_QUEEN),
+                new Move(initial, target, null, MoveCategory.PROMOTE_TO_ROOK),
+                new Move(initial, target, null, MoveCategory.PROMOTE_TO_BISHOP),
+                new Move(initial, target, null, MoveCategory.PROMOTE_TO_KNIGHT)
+            );
         } else {
-            return null;
+            return new ArrayList<>();
         }
     }
 
     @Override
-    public ArrayList<Move> getAdditionalLegalMoves(final Cell initial, final Position position, final Move lastMove) {
-        ArrayList<Move> list = new ArrayList<>();
+    public List<Move> getAdditionalLegalMoves(Cell initial, Position position, Move lastMove) {
+        List<Move> list = new ArrayList<>();
         // Double advance
         int initRow = SIZE - nextPromotion - 1;
         if (initial.getRow() == initRow) {
+            // Pawn hasn't moved yet
             Cell landingCell = initial.shift(dir * 2, 0);
             if (position.canOccupy(this, landingCell) && position.isFreePathBetween(initial, landingCell)) {
-                list.add(new Move(initial, landingCell));
+                list.add(new Move(initial, landingCell, position.get(initial)));
             }
         }
         if (lastMove == null) {
+            // No moves have been made in this game => other additional moves are definitely impossible
             return list;
         }
         // Pawn promotion
@@ -69,18 +91,13 @@ public class Pawn extends Piece {
             Cell a = initial.shift(dir, -1);
             Cell b = initial.shift(dir, 0);
             Cell c = initial.shift(dir, 1);
+            // If the pawn changes its column, then it must capture, otherwise it must not capture
             List<Move> left = checkPromotion(position.isOccupied(a), initial, a);
             List<Move> straight = checkPromotion(!position.isOccupied(b), initial, b);
             List<Move> right = checkPromotion(position.isOccupied(c), initial, c);
-            if (left != null) {
-                list.addAll(left);
-            }
-            if (straight != null) {
-                list.addAll(straight);
-            }
-            if (right != null) {
-                list.addAll(right);
-            }
+            list.addAll(left);
+            list.addAll(straight);
+            list.addAll(right);
         }
         // En passant
         Cell lastStart = lastMove.getStart();
@@ -90,7 +107,7 @@ public class Pawn extends Piece {
         if (position.adjacent(initial, lastTarget) && Math.abs(dr) == 2 && dc == 0) {
             int offset = lastStart.getCol() - initial.getCol();
             Cell landingCell = initial.shift(dir, offset);
-            list.add(new Move(initial, landingCell));
+            list.add(new Move(initial, landingCell, position.get(initial)));
         }
         return list;
     }
